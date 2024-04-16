231759429117f667ad0635f1a5c34c2322ec3b54
Chika Chijioke <8pluscreative@gmail.com>
2024-04-16 06:50:57 +0100

    Java Series

---
+++ Core/Variables/Variable.java
@@ -0,0 +1,26 @@
+class Variable 
+{
+	public static void main(String a[]){
+		int num1 = 5;
+		byte bte = 127;
+		short sh = 558;
+		long l = 5834l;
+
+		float f = 8.4f;
+		double d = 18.2;
+
+		char c = 'k';
+
+		boolean z = true;
+
+		// System.out.println(num1);
+		// System.out.println(bte);
+
+		// TYPE CONVERSION
+		int e = 342;
+		byte h = (byte) e;
+
+		System.out.println(h);
+		
+	}
+}
