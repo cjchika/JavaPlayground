9b275ebd0a9180eae408631edc252b0d30747270
Chika Chijioke <8pluscreative@gmail.com>
2024-04-18 09:24:33 +0100

    Java Series

--- Core/String/StringDemo.java
+++ Core/String/StringDemo.java
@@ -1,7 +1,6 @@
 public class StringDemo {
     public static void main(String[] args){
         String name = new String("Chika");
-        // int num = 5;
 
         System.out.println(name.charAt(2));
         System.out.println(name.contains("k"));
