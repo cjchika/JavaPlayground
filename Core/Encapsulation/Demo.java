ceb0554de9949d8da42322d33e8b90f77769a713
Chika Chijioke <8pluscreative@gmail.com>
2024-04-23 12:10:50 +0100

    Java Series

---
+++ Core/Encapsulation/Demo.java
@@ -0,0 +1,49 @@
+class Human 
+{
+  private int age;
+  private String name;
+  
+  public Human(int age, String name){
+    this.age = age;
+    this.name = name;
+  }
+
+  public int getAge(){
+    return this.age;
+  }
+
+  public void setAge(int a)
+  {
+    age = a;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setName(String n)
+  {
+    name = n;
+  }
+}
+
+
+public class Demo {
+
+  public static void main(String[] args) {
+
+    Human human1 = new Human(50, "Broski");
+    Human human2 = new Human(100, "Jide");
+    // human1.setAge(50);
+    // human1.setName("Broli");
+
+    System.out.println(human1.getName());
+    System.out.println(human1.getAge());
+
+    System.out.println(human2.getName());
+    System.out.println(human2.getAge());
+  }
+}
+
+
+
