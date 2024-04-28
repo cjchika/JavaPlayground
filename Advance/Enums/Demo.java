70d0a051520b97f3156b73ad1cc19ee59ca1838c
Chika Chijioke <8pluscreative@gmail.com>
2024-04-28 06:20:57 +0100

    Java Series

---
+++ Advance/Enums/Demo.java
@@ -0,0 +1,62 @@
+package Advance.Enums;
+
+enum Day {
+	MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
+
+enum Direction {
+	NORTH("N"), SOUTH("S"), EAST("E"), WEST("W");
+
+	private final String abbreviation;
+
+	Direction(String abbreviation) {
+		this.abbreviation = abbreviation;
+	}
+
+	public String getAbbreviation() {
+		return abbreviation;
+	}
+}
+
+public class Demo {
+	public static void main(String[] args) {
+
+		Day today = Day.SUNDAY;
+
+		switch (today) {
+			case MONDAY:
+				System.out.println("Today is Monday");
+				break;
+			case TUESDAY:
+				System.out.println("Today is Tuesday");
+				break;
+			case WEDNESDAY:
+				System.out.println("Today is Wedneday");
+				break;
+			case THURSDAY:
+				System.out.println("Today is Thursday");
+				break;
+			case FRIDAY:
+				System.out.println("Today is Friday");
+				break;
+			case SATURDAY:
+				System.out.println("Today is Saturday");
+				break;
+			case SUNDAY:
+				System.out.println("Today is Sunday");
+				break;
+			default:
+				System.out.println("Today is Monday");
+		}
+
+		printDay(today);
+
+		Direction direction = Direction.EAST;
+		System.out.println("Abbreviation for EAST is " + direction.getAbbreviation());
+	}
+
+	static void printDay(Day day) {
+		System.out.println(day);
+	}
+
+}
