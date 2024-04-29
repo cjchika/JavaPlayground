da03f6a88a4c657b0387141dbe5917259c3da733
Chika Chijioke <8pluscreative@gmail.com>
2024-04-29 09:23:42 +0100

    Java Series

--- Advance/Collection/Demo.java
+++ Advance/Collection/Demo.java
@@ -1,12 +1,14 @@
 package Advance.Collection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class Demo {
 	public static void main(String[] args) {
@@ -15,9 +17,9 @@ public class Demo {
 		fruits.add("Orange");
 		fruits.add("Pineapple");
 
-		// for (String fruit : fruits) {
-		// System.out.println(fruit);
-		// }
+		for (String fruit : fruits) {
+			System.out.println(fruit);
+		}
 
 		Set<Integer> nums = new HashSet<Integer>();
 		nums.add(20);
@@ -26,9 +28,9 @@ public class Demo {
 		nums.add(20);
 		nums.add(46);
 
-		// for (int num : nums) {
-		// System.out.println(num);
-		// }
+		for (int num : nums) {
+			System.out.println(num);
+		}
 
 		Map<String, Integer> students = new HashMap<>();
 
@@ -36,9 +38,9 @@ public class Demo {
 		students.put("Stephen", 72);
 		students.put("Lynn", 23);
 
-		// for (String key : students.keySet()) {
-		// System.out.println(key + " : " + students.get(key));
-		// }
+		for (String key : students.keySet()) {
+			System.out.println(key + " : " + students.get(key));
+		}
 
 		List<Integer> numbs = new ArrayList<>();
 		numbs.add(20);
@@ -49,6 +51,22 @@ public class Demo {
 
 		Collections.sort(numbs);
 
-		// System.out.println(numbs);
+		List<Integer> nrs = Arrays.asList(4, 2, 5, 1, 2, 9, 8);
+
+		Stream<Integer> ss1 = nrs.stream();
+		Stream<Integer> ss2 = ss1.filter(m -> m % 2 == 0);
+		Stream<Integer> ss3 = ss2.map(m -> m * 2);
+
+		ss3.forEach(n -> System.out.println(n));
+
+		int sum = 0;
+
+		for (int n : nrs) {
+			if (n % 2 == 0) {
+				n = n * 2;
+				sum += n;
+			}
+		}
+		System.out.println(sum);
 	}
 }
