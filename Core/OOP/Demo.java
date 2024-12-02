f0901599b8f9448fe480c888002e540b91c5e33f
Chika Chijioke <8pluscreative@gmail.com>
2024-12-02 18:16:31 +0100

    OOP

--- Core/OOP/Demo.java
+++ Core/OOP/Demo.java
@@ -19,14 +19,45 @@ public class Demo {
         Calculator calc = new Calculator();
         int res = calc.add(5,10);
 
-        System.out.println(res);
+//        System.out.println(res);
 
         Computer comp = new Computer();
         String brand = comp.getBrand();
 
-        System.out.println(brand);
+//        System.out.println(brand);
+
+        int nums[] = new int[3];
+        nums[0] = 50;
+        nums[1] = 100;
+        nums[2] = 150;
+
+//        for(int i = 0; i < nums.length; i++){
+//            System.out.println(nums[i]);
+//        }
+
+        int numsArr[][] = new int[3][5];
+
+        for(int i=0;i < 3; i++){
+            for(int j =0; j < 5; j++){
+               numsArr[i][j] = (int)(Math.random() * 10);
+            }
+        }
+
+        for(int i=0;i < 3; i++){
+            for(int j =0; j < 5; j++){
+                System.out.print(numsArr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Enhanced...");
+
+        for(int[] n : numsArr){
+            for(int m : n){
+                System.out.print(m + " ");
+            }
+            System.out.println();
+        }
     }
-
-
 
 }
--- Core/OOP/Main.java
+++ Core/OOP/Main.java
@@ -1,89 +1,23 @@
+class Phone {
+    private int price = 100;
+    private String brand = "iPhone";
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
+
 public class Main {
+
     public static void main(String[] args) {
-//        Human human = new Human("Bro", 28, 75);
-//        Human human2 = new Human("Sis", 25, 65);
-//
-//        System.out.println(human.name);
-//        System.out.println(human2.name);
-//
-//        human.drink();
-//        human2.eat();
+        Phone phone = new Phone();
 
-//        Cake cake = new Cake("The Flour", "The Sauce", "The Sugar");
-//
-//        System.out.println(cake.sugar);
-//        ----
-
-//        if(x < 8) myDog.bark(4);
-//
-//        while(x > 4){
-//            myDog.play();
-//        }
-
-//        int[] numList = {10,8,6,4};
-//        System.out.println("Hello");
-//        System.out.println("Dog: " + name);
-//        String num = "10";
-//        int converted = Integer.parseInt(num);
-//
-//        System.out.println(converted);
-
-//        int a = 1;
-//        System.out.println("Before the Loop");
-//        while(a < 4){
-//            System.out.println("In the loop");
-//            System.out.println("Value of x is " + a);
-//            a = a + 1;
-//        }
-//        System.out.println("After the Loop.");
-
-//        -----
-
-//        int beerNum = 99;
-//        String word = "bottles";
-//
-//        while(beerNum > 0){
-//            if(beerNum == 1){
-//                word = "bottle";
-//            }
-//
-//            System.out.println(beerNum + " " + word + " of beer on the wall");
-//            System.out.println(beerNum + " " + word + " of beer.");
-//            System.out.println("Take one down.");
-//            System.out.println("Pass it around");
-//            beerNum = beerNum - 1;
-//
-//            if(beerNum == 0) {
-//                System.out.println("No more bottles of beer on the wall");
-//            }
-//        }
-
-//        -----
-
-//        Dog myDog = new Dog();
-//        myDog.age = 10;
-//        myDog.name = "Champion";
-//        myDog.breed = "German Shepherd";
-//
-//        myDog.bark();
-
-        Dog[] pets;
-
-        pets = new Dog[3];
-
-        pets[0] = new Dog();
-        pets[1] = new Dog();
-        pets[2] = new Dog();
-
-        pets[0].setName("Tiger");
-
-        pets[1].setName("Champion");
-
-        pets[2].setName("Rough");
-
-        for(int i = 0; i < pets.length; i++){
-            pets[i].bark(3);
-        }
-        System.out.println(pets[0].getName());
+        phone.setBrand("Samsung");
+        String ph1 = phone.getBrand();
+        System.out.println(ph1);
     }
 }
