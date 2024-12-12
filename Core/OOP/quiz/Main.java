b17a194373de0c86add94a52c5758aba0d28b2ad
Chika Chijioke <8pluscreative@gmail.com>
2024-12-12 19:45:43 +0100

    OOP

--- Core/OOP/quiz/Main.java
+++ Core/OOP/quiz/Main.java
@@ -1,8 +1,40 @@
 package quiz;
 
+class A extends Thread{
+    public void run(){
+      for (int i =1; i <=10;i++){
+          System.out.println("Hi");
+          try {
+              Thread.sleep(10);
+          } catch (InterruptedException e) {
+              throw new RuntimeException(e);
+          }
+      }
+    }
+}
+
+class B extends Thread{
+    public void run(){
+        for (int i =1; i <=10;i++){
+            System.out.println("Hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         QuestionService svc = new QuestionService();
-        svc.playQuiz();
+//        svc.playQuiz();
+
+        A obj1 = new A();
+        B obj2 = new B();
+
+        obj1.start();
+        obj2.start();
     }
 }
