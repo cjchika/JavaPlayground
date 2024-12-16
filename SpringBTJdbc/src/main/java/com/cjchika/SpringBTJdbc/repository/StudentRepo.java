06cc3a9c3d27ce3def92f13c5f5f551a8d26cdff
Chika Chijioke <8pluscreative@gmail.com>
2024-12-16 12:29:19 +0100

    SpringBoot JDBC

--- SpringBTJdbc/src/main/java/com/cjchika/SpringBTJdbc/repository/StudentRepo.java
+++ SpringBTJdbc/src/main/java/com/cjchika/SpringBTJdbc/repository/StudentRepo.java
@@ -3,8 +3,11 @@ package com.cjchika.SpringBTJdbc.repository;
 import com.cjchika.SpringBTJdbc.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,7 +35,27 @@ public class StudentRepo {
     }
 
     public List<Student> findAll(){
-        List<Student> students = new ArrayList<>();
-        return students;
+
+        String query = "select * from student";
+
+        return jdbc.query(query, (rs, rowNum) -> {
+            Student s = new Student();
+            s.setRollNo(rs.getInt("rollNo"));
+            s.setName(rs.getString("name"));
+            s.setMarks(rs.getInt("marks"));
+
+            return s;
+        });
+
+//        RowMapper<Student> mapper = (ResultSet rs, int rowNum) -> {
+//                Student s = new Student();
+//                s.setRollNo(rs.getInt("rollNo"));
+//                s.setName(rs.getString("name"));
+//                s.setMarks(rs.getInt("marks"));
+//
+//                return s;
+//        };
+
+//        return  jdbc.query(query, mapper);
     }
 }
