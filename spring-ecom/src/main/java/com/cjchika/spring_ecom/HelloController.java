c6839d21113699a0b72f75d9409cf61382ca50b1
Chika Chijioke <8pluscreative@gmail.com>
2024-12-20 09:20:11 +0100

    Spring Ecommerce App REST API

---
+++ spring-ecom/src/main/java/com/cjchika/spring_ecom/HelloController.java
@@ -0,0 +1,15 @@
+package com.cjchika.spring_ecom;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
+}
