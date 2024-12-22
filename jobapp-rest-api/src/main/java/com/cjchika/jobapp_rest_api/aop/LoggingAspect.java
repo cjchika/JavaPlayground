6e8f9acac24f0e49d12c573cf8976b061aab5fc1
Chika Chijioke <8pluscreative@gmail.com>
2024-12-22 07:48:35 +0100

    Spring AOP

--- jobapp-rest-api/src/main/java/com/cjchika/jobapp_rest_api/aop/LoggingAspect.java
+++ jobapp-rest-api/src/main/java/com/cjchika/jobapp_rest_api/aop/LoggingAspect.java
@@ -1,13 +1,27 @@
 package com.cjchika.jobapp_rest_api.aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+
+@Component
+@Aspect
 public class LoggingAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
-    public void logMethodCall(){
-        LOGGER.info("Method called!");
+    @Before("execution(* com.cjchika.jobapp_rest_api.service.JobService.getAllJobs*(..))")
+    public void logMethodCall(JoinPoint jp){
+        LOGGER.info("Method called!" + jp.getSignature().getName());
+    }
+
+    @After("execution(* com.cjchika.jobapp_rest_api.service.JobService.getAllJobs*(..))")
+    public void logMethodExecuted(JoinPoint jp){
+        LOGGER.info("Method Executed!" + jp.getSignature().getName());
     }
 }
