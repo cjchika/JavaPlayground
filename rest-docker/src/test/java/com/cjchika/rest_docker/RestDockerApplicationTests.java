b7e0deb9730934c690e9d8e2260d01cb2292c1b2
Chika Chijioke <8pluscreative@gmail.com>
2024-12-24 06:28:33 +0100

    Spring REST Docker

---
+++ rest-docker/src/test/java/com/cjchika/rest_docker/RestDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjchika.rest_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
